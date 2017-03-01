66cfc5242c7e53b5647e6428fe0dffd1f6e5c4b4
Eeliya Rasta <eeliya.rasta@gmail.com>
2017-03-01 16:44:26 +0100

    Improvements

--- galaxyjs/galaxy.js
+++ galaxyjs/galaxy.js
@@ -832,10 +832,10 @@
     this.active = true;
     this.trigger('onStart');
     //this.triggerEvent('start');
-    if (('system/' + this.domain.app.params[this.stateKey]).indexOf(this.id) <= -1) {
-      console.log(this.domain.app.params[this.stateKey]);
-      throw new Error('Could not find module `' + this.id + '` by state key `' + this.stateKey + '`');
-    }
+    // if (('system/' + this.domain.app.params[this.stateKey]).indexOf(this.id) <= -1) {
+    //   console.log(this.domain.app.params[this.stateKey]);
+    //   throw new Error('Could not find module `' + this.id + '` by state key `' + this.stateKey + '`');
+    // }
     var newNav = Galaxy.utility.extend(true, {}, this.domain.app.navigation);
     var st = 'system/' + this.domain.app.params[this.stateKey];
     var napPath = st.indexOf(this.id) === 0 ? st.substr(this.id.length).split('/').filter(Boolean) : [];
@@ -1107,6 +1107,7 @@
   GalaxyModule.prototype.hashHandler = function (nav, params) {};
 
 })();
+
 // https://github.com/yanatan16/nanoajax
 !function(t,e){function n(t){return t&&e.XDomainRequest&&!/MSIE 1/.test(navigator.userAgent)?new XDomainRequest:e.XMLHttpRequest?new XMLHttpRequest:void 0}function o(t,e,n){t[e]=t[e]||n}var r=["responseType","withCredentials","timeout","onprogress"];t.ajax=function(t,a){function s(t,e){return function(){c||(a(void 0===f.status?t:f.status,0===f.status?"Error":f.response||f.responseText||e,f),c=!0)}}var u=t.headers||{},i=t.body,d=t.method||(i?"POST":"GET"),c=!1,f=n(t.cors);f.open(d,t.url,!0);var l=f.onload=s(200);f.onreadystatechange=function(){4===f.readyState&&l()},f.onerror=s(null,"Error"),f.ontimeout=s(null,"Timeout"),f.onabort=s(null,"Abort"),i&&(o(u,"X-Requested-With","XMLHttpRequest"),e.FormData&&i instanceof e.FormData||o(u,"Content-Type","application/x-www-form-urlencoded"));for(var p,m=0,v=r.length;v>m;m++)p=r[m],void 0!==t[p]&&(f[p]=t[p]);for(var p in u)f.setRequestHeader(p,u[p]);return f.send(i),f},e.nanoajax=t}({},function(){return this}());
 /* global Galaxy */
