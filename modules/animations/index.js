498fb46066678b8baee157f9cced167d181e3ca2
Eeliya <eeliya.rasta@gmail.com>
2018-10-05 00:37:46 +0200

    WIP: Animations section

--- modules/animations/index.js
+++ modules/animations/index.js
@@ -1,4 +1,58 @@
 const view = Scope.import('galaxy/view');
 const css = Scope.import('./style.css');
 
-view.style(css);
+Scope.data.boxes = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'
+];
+
+view.init([
+  {
+    class: 'card',
+    animations: {
+      enter: {
+        sequence: 'card',
+        startAfter: 'main-nav-items',
+
+        from: {
+          x: 100,
+          y: 150,
+          rotationZ: -20,
+          opacity: 0
+        },
+        duration: .5
+      }
+    },
+    children: [
+      {
+        tag: 'section',
+        class: 'content',
+        children: [
+          {
+            tag: 'h1',
+            text: 'Animations'
+          },
+          {
+            animations: {
+              enter: {
+                sequence: 'boxes',
+                startAfter: 'card',
+
+                from: {
+                  scale: 0
+                },
+                duration: .4,
+                position: '-=.3'
+              }
+            },
+            class: 'box',
+            $for: {
+              data: '<>data.boxes.changes',
+              as: 'item'
+            },
+            text: '<>item'
+          }
+        ]
+      }
+    ]
+  }
+]);
