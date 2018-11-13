3a2cf1151726c144928b328fdf4968498a4348b4
Eeliya Rasta <eeliya.rasta@gmail.com>
2018-11-13 17:29:42 +0100

    WIP: Improvements to router add-on.

--- galaxyjs/galaxy.js
+++ galaxyjs/galaxy.js
@@ -6246,6 +6246,54 @@ Galaxy.View.ViewNode = /** @class */ (function (GV) {
 
 
 /* global Galaxy */
+'use strict';
+
+(function (GV) {
+  GV.NODE_SCHEMA_PROPERTY_MAP['inputs'] = {
+    type: 'reactive',
+    name: 'inputs'
+  };
+
+  GV.REACTIVE_BEHAVIORS['inputs'] = {
+    regex: null,
+    /**
+     *
+     * @this {Galaxy.View.ViewNode}
+     * @param matches
+     * @param scope
+     */
+    prepare: function (matches, scope) {
+      if (matches !== null && typeof  matches !== 'object') {
+        throw console.error('inputs property should be an object with explicits keys:\n', JSON.stringify(this.schema, null, '  '));
+      }
+
+      return {
+        subjects: matches,
+        scope: scope
+      };
+    },
+    /**
+     *
+     * @this {Galaxy.View.ViewNode}
+     * @param data
+     * @return {boolean}
+     */
+    install: function (data) {
+      if (this.virtual) {
+        return;
+      }
+
+      const reactive = GV.bindSubjectsToData(this, data.subjects, data.scope, true);
+
+      this.inputs = reactive;
+
+      return false;
+    },
+    apply: function (cache, value, oldValue, context) { }
+  };
+})(Galaxy.View);
+
+/* global Galaxy */
 
 (function (Galaxy) {
   Galaxy.View.NODE_SCHEMA_PROPERTY_MAP['module'] = {
@@ -6695,65 +6743,20 @@ Galaxy.View.PROPERTY_SETTERS.prop = function (viewNode, attrName, property, expr
 /* global Galaxy */
 'use strict';
 
-(function (GV) {
-  GV.NODE_SCHEMA_PROPERTY_MAP['inputs'] = {
-    type: 'reactive',
-    name: 'inputs'
-  };
-
-  GV.REACTIVE_BEHAVIORS['inputs'] = {
-    regex: null,
-    /**
-     *
-     * @this {Galaxy.View.ViewNode}
-     * @param matches
-     * @param scope
-     */
-    prepare: function (matches, scope) {
-      if (matches !== null && typeof  matches !== 'object') {
-        throw console.error('inputs property should be an object with explicits keys:\n', JSON.stringify(this.schema, null, '  '));
-      }
-
-      return {
-        subjects: matches,
-        scope: scope
-      };
-    },
-    /**
-     *
-     * @this {Galaxy.View.ViewNode}
-     * @param data
-     * @return {boolean}
-     */
-    install: function (data) {
-      if (this.virtual) {
-        return;
-      }
-
-      const reactive = GV.bindSubjectsToData(this, data.subjects, data.scope, true);
-
-      this.inputs = reactive;
-
-      return false;
-    },
-    apply: function (cache, value, oldValue, context) { }
-  };
-
-  Galaxy.registerAddOnProvider('galaxy/inputs', function (scope) {
+(function (G) {
+  G.registerAddOnProvider('galaxy/inputs', function (scope) {
     return {
       /**
        *
        * @return {*}
        */
       create: function () {
-        // scope.inputs = scope.element.cache.inputs.reactive;
-        // scope.inputs = scope.element.inputs;
         return scope.inputs;
       },
       finalize: function () { }
     };
   });
-})(Galaxy.View);
+})(Galaxy);
 
 (function (G) {
   'use strict';
@@ -6869,6 +6872,8 @@ Galaxy.View.PROPERTY_SETTERS.prop = function (viewNode, attrName, property, expr
           _this.oldResolveId[dynamicRoute.id] = resolveId;
           _this.routes[dynamicRoute.id].call(null, params);
           parentRoute = dynamicRoute;
+          // Routes order is important and longer route should have higher priority
+          break;
         }
       }
 
